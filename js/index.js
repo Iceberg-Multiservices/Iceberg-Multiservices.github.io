983db7c941f2bbd60e723672ea96af9de1570c20
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2022-10-30 13:34:14 -0500

    Create markup and style for Language Window.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 $(document).ready(function () {
     var activePortfolioId = 'portfolio-1';
 
+    $(window).keyup(function (event) {
+        if (event.key == 'Escape') {
+            closeWindow();
+        }
+    });
+
+    $('button.switch-language').click(function (event) {
+        showWindow();
+    });
+
+    $('section.popup-bg').mousedown(function (event) {
+        closeWindow();
+    });
+
+    $('section.popup-bg .window').mousedown(function (event) {
+        event.stopPropagation();
+    });
+
+    $('button.close-window').click(function (event) {
+        closeWindow();
+    });
+
     /**
      * Bullet button for Portfolio.
      */
@@ -19,4 +41,12 @@ $(document).ready(function () {
             $('#' + activePortfolioId).fadeIn('slow');
         });
     });
+
+    function showWindow() {
+        $('section.popup-bg').css('display', 'flex');
+    }
+
+    function closeWindow() {
+        $('section.popup-bg').css('display', 'none');
+    }
 });
